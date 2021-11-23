3e89ba93560e41fd1edbe564a0b486c7f19b83e8
708u <708.u.biz@gmail.com>
2021-11-23 17:13:53 +0900

    feat: instal fast syntax highlighting

--- zsh/plugin.zsh
+++ zsh/plugin.zsh
@@ -8,3 +8,5 @@ zinit light romkatv/powerlevel10k
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
+
+zinit light zdharma-continuum/fast-syntax-highlighting
