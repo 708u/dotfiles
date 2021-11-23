b2a231f662918a31a286874cdb87fa6a101a9484
708u <708.u.biz@gmail.com>
2021-11-23 20:16:21 +0900

    feat: add select history

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -4,6 +4,8 @@ if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
   export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
 fi
 
+export FZF_DEFAULT_OPTS='--reverse'
+
 # Auto-completion
 # ---------------
 [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
@@ -11,3 +13,11 @@ fi
 # Key bindings
 # ------------
 source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+
+# fzf履歴検索
+function select-history() {
+ BUFFER=$(history -n -r 1 | fzf --no-sort +m --query "$LBUFFER" --prompt="History > ")
+ CURSOR=$#BUFFER
+}
+zle -N select-history
+bindkey '^r' select-history
